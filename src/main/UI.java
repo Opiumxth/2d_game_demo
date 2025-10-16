310a60f43efc8263d74a4c5bf453d3ae418086d0
Opiumxth <aki.chainsaw24@gmail.com>
2025-10-15 23:40:01 -0500

    Add comments to UI class

--- src/main/UI.java
+++ src/main/UI.java
@@ -8,10 +8,11 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class UI {
-    GamePanel gp;
-    Font arial_40;
-    BufferedImage keyImage;
+    GamePanel gp; // Reference to the main game panel
+    Font arial_40; // Font for the ui text
+    BufferedImage keyImage; // Key icon image
 
+    // Constructor, this initializes the ui elements
     public UI(GamePanel gp){
         this.gp = gp;
         arial_40 = new Font("Arial", Font.BOLD, 40);
@@ -19,11 +20,11 @@ public class UI {
         keyImage = key.image;
     }
 
+    // Draws ui elements on screen, used in GamePanel class
     public void draw(Graphics2D g2){
         g2.setFont(arial_40);
         g2.setColor(Color.WHITE);
         g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
         g2.drawString("x " + gp.player.hasKey, 74, 65);
     }
-
 }
